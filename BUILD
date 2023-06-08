9d83edb6448bbe790c6cb2ee55070a60b7839bba
balachandarg-tw <balachandar.g@thoughtworks.com>
2023-06-08 10:19:45 +0530

    Initial commit for Bazel build spikes

---
+++ BUILD
@@ -0,0 +1,8 @@
+android_binary(
+    name = "app",
+    custom_package = "com.balagtw.bazelkotlin",
+    manifest = "AndroidManifest.xml",
+    deps = [
+    "//src:lib"
+    ],
+)
---
+++ src/java/com/balagtw/bazelkotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.balagtw.bazelkotlin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+    class MainActivity : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            this.setContentView(R.layout.activity_main)
+        }
+
+    }
